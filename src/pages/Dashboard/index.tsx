5e087ee691628791ede75c9d1e972d8386992f1e
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-14 16:26:55 -0300

    Fetch list food and set state

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const { data } = await api.get('/foods');
+      setFoods(data);
     }
 
     loadFoods();
